b75730000000bef224f6d37b25aa880b64942a48
Scott Schwab <scottschwab86@gmail.com>
2024-01-30 11:53:09 -0600

    Finish challenge

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,11 +1,48 @@
 function selectionSort(arr) {
   // type your code here
+
+  // have a sortedArr = []
+  // iterate over arr
+    // find min element
+    // push that min element to sortedArr
+    // splice that element out of arr
+  //return sortedArr
+
+  let sortedArr = [];
+
+  let arrLength = arr.length;
+
+  for (let i = 0; i < arrLength; i++) {
+    let smallest = Math.min(...arr);
+    let idx = arr.indexOf(smallest);
+    sortedArr.push(Math.min(...arr));
+    arr.splice(idx, 1);
+  }
+
+  return sortedArr
+
 }
+
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSort([3, -1, 5, 2]));
+
+  console.log("");
+
+  console.log("Expecting: [1, 3, 5, 5, 10]");
+  console.log("=>", selectionSort([5, 3, 1, 5, 10]));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", selectionSort([]));
+
+  console.log("");
+
+  console.log("Expecting: [5, 5, 5]");
+  console.log("=>", selectionSort([5, 5, 5]));
 
   console.log("");
 
@@ -15,6 +52,19 @@ if (require.main === module) {
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
+
+  const shortInput = [3, 1, 2];
+
+  let startTime = Date.now();
+
+  for (let j = 0; j < 1000; j++) {
+    selectionSort(shortInput);
+    selectionSort(longInput);
+  }
+
+  let averageRuntime = (Date.now() - startTime) / 2000;
+
+  console.log(averageRuntime)
 }
 
 module.exports = selectionSort;
